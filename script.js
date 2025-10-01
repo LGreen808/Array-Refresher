c8afc97384a092915e946fe949e90a5a0e821239
LGreen8084 <526LGreen@frhsd.com>
2025-10-01 09:14:35 -0400

    Third Commit

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-const listArray = ["Pasta",
+const listArray = ["The Pirate", "Carry On", "Rocketman", "Elvis", "The Irishman", "Unbroken", "Red Eye", "The Immortal Man", "Oppenhimer", "Guys and Dolls"
