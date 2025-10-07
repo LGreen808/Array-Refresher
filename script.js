30defc981cc374e14f44be72e75bac487671b389
LGreen808 <526LGreen@frhsd.com>
2025-10-07 21:45:20 +0000

    Fourth commit

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-const listArray = ["The Pirate", "Carry On", "Rocketman", "Elvis", "The Irishman", "Unbroken", "Red Eye", "The Immortal Man", "Oppenhimer", "Guys and Dolls"
+const listArray = ["The Pirate", "Carry On", "Rocketman", "Elvis", "The Irishman", "Unbroken", "Red Eye", "The Immortal Man", "Oppenhimer", "Guys and Dolls"]
+
+const arrayContainer = document.getElementById("output");
+
+listArray.forEach (item => {
+    const listItem = document.createElementById("li");
+    listItem.textContent = item;
+    listContainer.appendChild(listItem); 
+});
